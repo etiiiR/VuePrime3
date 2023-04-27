51cd66a176967ab6fc3a351d1fba58ba9d10d05c
Etienne Roulet <etienne.roulet@outlook.com>
2023-04-27 13:09:14 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,115 @@
+
+<template>
+	<div class="card p-fluid">
+        <DataTable :value="customers" lazy paginator :rows="10" v-model:filters="filters" ref="dt" dataKey="id"
+            :totalRecords="totalRecords" :loading="loading" @page="onPage($event)" @sort="onSort($event)" @filter="onFilter($event)" filterDisplay="row"
+            :globalFilterFields="['name','country.name', 'company', 'representative.name']"
+            v-model:selection="selectedCustomers" :selectAll="selectAll" @select-all-change="onSelectAllChange" @row-select="onRowSelect" @row-unselect="onRowUnselect" tableStyle="min-width: 75rem">
+            <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
+            <Column field="name" header="Name" filterMatchMode="startsWith" sortable>
+                <template #filter="{filterModel,filterCallback}">
+                    <InputText type="text" v-model="filterModel.value" @keydown.enter="filterCallback()" class="p-column-filter" placeholder="Search"/>
+                </template>
+            </Column>
+            <Column field="country.name" header="Country" filterField="country.name" filterMatchMode="contains" sortable>
+                <template #filter="{filterModel,filterCallback}">
+                    <InputText type="text" v-model="filterModel.value" @keydown.enter="filterCallback()" class="p-column-filter" placeholder="Search"/>
+                </template>
+            </Column>
+            <Column field="company" header="Company" filterMatchMode="contains" sortable>
+                <template #filter="{filterModel,filterCallback}">
+                    <InputText type="text" v-model="filterModel.value" @keydown.enter="filterCallback()" class="p-column-filter" placeholder="Search"/>
+                </template>
+            </Column>
+            <Column field="representative.name" header="Representative" filterField="representative.name" sortable>
+                <template #filter="{filterModel,filterCallback}">
+                    <InputText type="text" v-model="filterModel.value" @keydown.enter="filterCallback()" class="p-column-filter" placeholder="Search"/>
+                </template>
+            </Column>
+        </DataTable>
+	</div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { CustomerService } from '@/service/CustomerService';
+
+onMounted(() => {
+    loading.value = true;
+
+    lazyParams.value = {
+        first: 0,
+        rows: dt.value.rows,
+        sortField: null,
+        sortOrder: null,
+        filters: filters.value
+    };
+
+    loadLazyData();
+});
+
+const dt = ref();
+const loading = ref(false);
+const totalRecords = ref(0);
+const customers = ref();
+const selectedCustomers = ref();
+const selectAll = ref(false);
+const filters = ref({
+    'name': {value: '', matchMode: 'contains'},
+    'country.name': {value: '', matchMode: 'contains'},
+    'company': {value: '', matchMode: 'contains'},
+    'representative.name': {value: '', matchMode: 'contains'},
+});
+const lazyParams = ref({});
+const columns = ref([
+    {field: 'name', header: 'Name'},
+    {field: 'country.name', header: 'Country'},
+    {field: 'company', header: 'Company'},
+    {field: 'representative.name', header: 'Representative'}
+]);
+
+const loadLazyData = () => {
+    loading.value = true;
+
+    setTimeout(() => {
+        CustomerService.getCustomers({ lazyEvent: JSON.stringify(lazyParams.value) }).then((data) => {
+            customers.value = data.customers;
+            totalRecords.value = data.totalRecords;
+            loading.value = false;
+        });
+    }, Math.random() * 1000 + 250);
+};
+const onPage = (event) => {
+    lazyParams.value = event;
+    loadLazyData();
+};
+const onSort = (event) => {
+    lazyParams.value = event;
+    loadLazyData();
+};
+const onFilter = () => {
+    lazyParams.value.filters = filters.value ;
+    loadLazyData();
+};
+const onSelectAllChange = (event) => {
+    selectAll.value = event.checked;
+
+    if (selectAll) {
+        CustomerService.getCustomers().then(data => {
+            selectAll.value = true;
+            selectedCustomers.value = data.customers;
+        });
+    }
+    else {
+        selectAll.value = false;
+        selectedCustomers.value = [];
+    }
+};
+const onRowSelect = () => {
+    selectAll.value = selectedCustomers.value.length === totalRecords.value;
+};
+const onRowUnselect = () => {
+    selectAll.value = false;
+};
+
+</script>
